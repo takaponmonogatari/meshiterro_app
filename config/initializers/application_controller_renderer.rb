d0892eeaa1e7dbd4875b14c3e44b2b8490ecd46d
takaponmonogatari <taka11hiro26@gmail.com>
2021-04-14 02:20:12 +0000

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c3d6ace1449342faff09d7c91a10aab488f4202bd110e00c837df91b8bd79c70d91796250ee92e83be23293e5cbf0ad081a8a5b8ac8628012a742f412293396e'
