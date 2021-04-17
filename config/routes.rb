1d89938ecc646a1b51b7ea962096aa1c5feb0d00
takaponmonogatari <taka11hiro26@gmail.com>
2021-04-17 01:07:49 +0000

    [Finish] 5-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#top"
+  root to: 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
 
